c72204a81ad4f57ae69f321c0135a1e7a4d151d8
vipul <vipsviper2@gmail.com>
2026-07-31 06:15:13 +0530

    Time: 3 ms (52.35%), Space: 11.7 MB (74.98%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+        
+        for(char c : magazine) {
+            freq[c - 'a']++;
+        }
+        
+        for(char c : ransomNote) {
+            if(freq[c - 'a'] == 0) return false;
+            freq[c - 'a']--;
+        }
+        
+        return true;
+    }
+};
